cb3749e7587db6deab1e751741039a1443f002a2
PermaiRamadhan <rdadan130@gmail.com>
2021-05-29 10:55:02 +0700

    Initial commit

---
+++ Quiz10118060/app/src/main/java/com/dadan/quiz10118060/MainActivity.java
@@ -0,0 +1,20 @@
+package com.trizdev.quiz10118060;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//<!--tanggal pengerjaan : 29/05/2021-->
+//<!--Nim :10118060-->
+//<!--Nama :Permai Ramadhan-->
+//<!--Kelas : IF2/S1/VI-->
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
